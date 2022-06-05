859e4d930ec42e0405afacda908b4d9863a0d66f
Bruno Igor <brigor7@gmail.com>
2022-06-05 15:04:29 -0300

    Added addition method for doubles

--- Calculator.java
+++ Calculator.java
@@ -3,9 +3,14 @@ public class Calculator{
 		return a + b;
 	}
 	
+	public double addition(double a, double b){
+		return a + b;
+	}
+	
 	public static void main(String a[]){
 		Calculator calculator = new Calculator();
 		System.out.println("Projeto Calculadora");
 		System.out.println("Addition 2 + 3 = "+calculator.addition(2,3));
+		System.out.println("Addition 2.5 + 3.5 = "+calculator.addition(2.5,3.5));
 	}
 }
