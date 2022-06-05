d287c665f6757a22bdd848b9993ee253c3530354
Bruno Igor <brigor7@gmail.com>
2022-06-05 07:50:58 -0300

    Adding a method to Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,11 @@
 public class Calculator{
+	public int addition(int a, int b){
+		return a + b;
+	}
+	
 	public static void main(String a[]){
+		Calculator calculator = new Calculator();
 		System.out.println("Projeto Calculadora");
+		System.out.println("Addition 2 + 3 = "+calculator.addition(2,3));
 	}
 }
